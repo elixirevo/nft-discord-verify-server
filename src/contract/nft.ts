bbd082cad264e9d0949fa6c8133b9b96fce7206c
elixirevo <elixirevocation@gmail.com>
2022-03-23 18:14:41 +0900

    fix: ca

--- src/contract/nft.ts
+++ src/contract/nft.ts
@@ -2,7 +2,7 @@ import { ethers } from 'ethers';
 import ERC721ABI from '../../abi/ERC721.json';
 import { klaytnProvider } from '../chain/index';
 
-const contractAddress = '0x19FEb74A239fC7D6583d8C434A388880747E8dff';
+const contractAddress = '';
 
 export async function balanceOf(address: string) {
   const nft = new ethers.Contract(contractAddress, ERC721ABI, klaytnProvider);
